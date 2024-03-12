c177bfc0c25af2b31473b3ac04ecaa1d8b167a7e
Vibha V Joshi <73216270+vibhajoshi@users.noreply.github.com>
2024-03-12 21:08:10 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,33 @@
+
+def turn_right():
+   turn_left()
+   turn_left()
+   turn_left()
+
+def jump():
+    for i in range(6):
+        move()
+        turn_left()
+        move()
+        turn_right()
+        move()
+        turn_right()
+        move()
+        turn_left()
+
+jump()
+
+
+
+
+
+
+
+
+
+    
+    
+   
+    
+
+

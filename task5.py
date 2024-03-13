896babd601d40ee9fb234b524f6536b9ecf7bfd2
Vibha V Joshi <73216270+vibhajoshi@users.noreply.github.com>
2024-03-13 20:47:59 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,53 @@
+
+def turn_right():
+   turn_left()
+   turn_left()
+   turn_left()
+
+def jump():  
+        turn_left()
+        move()
+        while wall_on_right():
+            move()
+        turn_right()
+        move()
+        turn_right()
+        while front_is_clear():
+            move()
+        turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+        
+while at_goal()==0:
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+
+    
+   
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+   
+    
+
+
